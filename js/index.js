9663ab88b05226269a78c1abab9ff1e15cb899e1
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-22 00:44:30 +0600

    news categories done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("js connected");
+const loadCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+  };
+  
+  const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById("categories-container");
+    categoriesContainer.style.display = 'grid';
+    categoriesContainer.style.gridTemplateColumns = 'repeat(auto-fit, minmax(20%, 2fr))'; // Adjust the column width as desired
+    categoriesContainer.style.gridGap = '10px'; // Add spacing between categories
+  
+    categories.forEach((category) => {
+      const categoriesDiv = document.createElement('div');
+      categoriesDiv.innerHTML = `<p>${category.category_name}</p>`;
+      categoriesContainer.appendChild(categoriesDiv);
+    });
+  };
+  
+  loadCategories();
