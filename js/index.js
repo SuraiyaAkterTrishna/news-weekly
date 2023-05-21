fade2f191fb518e8d2e895f37ef5aa54cf3b2144
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-22 01:21:36 +0600

    load category items done

--- js/index.js
+++ js/index.js
@@ -7,15 +7,30 @@ const loadCategories = async () => {
   
   const displayCategories = (categories) => {
     const categoriesContainer = document.getElementById("categories-container");
-    categoriesContainer.style.display = 'grid';
-    categoriesContainer.style.gridTemplateColumns = 'repeat(auto-fit, minmax(20%, 2fr))'; // Adjust the column width as desired
-    categoriesContainer.style.gridGap = '10px'; // Add spacing between categories
+  
+    categoriesContainer.style.display = 'flex';
   
     categories.forEach((category) => {
+        // console.log(category);
       const categoriesDiv = document.createElement('div');
-      categoriesDiv.innerHTML = `<p>${category.category_name}</p>`;
+      categoriesDiv.style.flexGrow = 'auto'; // Adjust the flex properties as desired
+      categoriesDiv.style.flexShrink = '0';
+      categoriesDiv.style.marginBottom = '20px'; // Add spacing between categories
+      categoriesDiv.style.flexWrap = 'wrap';
+      categoriesDiv.innerHTML = `<button onclick="loadCategoryItems(${category.category_id})" class="border-0 bg-white">${category.category_name}</button>`;
       categoriesContainer.appendChild(categoriesDiv);
     });
   };
-  
+  const loadCategoryItems = async (category_id) => {
+    // console.log(category_id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategoryItems(data);
+  };
+  const displayCategoryItems = (items) => {
+    console.log(items);
+  }
   loadCategories();
+  
+  
