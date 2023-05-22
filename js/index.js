752ceb60941860e86f170003e94748eda841762c
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-22 17:02:18 +0600

    dynamically load items cards

--- js/index.js
+++ js/index.js
@@ -23,27 +23,50 @@ const displayCategories = (categories) => {
 };
 const loadCategoryItems = async (category_id) => {
   const id = category_id.toString().padStart(2, "0");
-  console.log(id);
+  // console.log(id);
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   const res = await fetch(url);
   const data = await res.json();
   displayCategoryItems(data.data);
 };
 const displayCategoryItems = (items) => {
-  console.log(items);
+  // console.log(items);
   const totalItems = document.getElementById("total-items");
 
   if (items.length) {
-    totalItems.classList.remove('d-none');
+    totalItems.classList.remove("d-none");
     totalItems.innerHTML = `
     <p class="py-2 px-2">${items.length} items found</p>
     `;
-  }
-  else {
-    totalItems.classList.remove('d-none');
+  } else {
+    totalItems.classList.remove("d-none");
     totalItems.innerHTML = `
     <p class="py-2 px-2">No available items found</p>
     `;
   }
+  const itemsContainer = document.getElementById("itemsContainer");
+  itemsContainer.innerHTML = ``;
+  items.forEach((item) => {
+    // console.log(item);
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("col");
+    itemDiv.innerHTML = `
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+    itemsContainer.appendChild(itemDiv);
+  });
 };
 loadCategories();
